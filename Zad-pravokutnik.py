812af2419bc7418297956cbee4b809a475d1e6b6
asimac <simac.andrea@gmail.com>
2025-01-26 15:58:41 +0100

    Zad-pravokutnik

---
+++ Zad-pravokutnik.py
@@ -0,0 +1,10 @@
+#Napiši program koji će učitati dvije katete pravokutnog trokuta i vratiti hipotenuzu:
+#Unesi katetu a? 3
+#Unesi katetu b? 4
+#Kateta iznosi 5
+
+#Formula za racunanje hipotenuze c2= a2 + b2
+
+a = float (input ("Unesi vrijednost katete a: "))
+b = float (input ("Unesi vrijednost katete b: "))
+c= print ("Vrijednost hipotenuce c:",(a**2 + b**2)**0.5)
