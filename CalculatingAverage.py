4a848182ad5f33d2b69448c4455d2c050c8b4634
asimac <simac.andrea@gmail.com>
2025-02-16 15:25:48 +0100

    Adding Students list

--- CalculatingAverage.py
+++ CalculatingAverage.py
@@ -2,9 +2,10 @@
 # and will use the input of desired average to calculate the grade(s) needed to reach that average!
 # Program will also check if the inputs are valid or not. Program will ask User for: ID, Name, Last Name and Grades.
 
-# ENTER NESSESARY INPUTS
+# ENTER NECESSARY INPUTS
 import math
 
+students = []
 
 while True:
     full_name = input ("Enter your First and Last Name: ").strip()
@@ -12,7 +13,7 @@ while True:
         first_name, last_name = full_name.split(maxsplit= 1)
         if first_name.istitle() and last_name.istitle():
             break
-    print ("Invalid value! First latter of the Name and the Last name should be capital!")
+    print ("Invalid input! First letter of the First Name and the Last Name should be capitalized!")
 
 
 while True:
@@ -22,10 +23,10 @@ while True:
         break
     print("Invalid value! Please enter a six digit number!")
 
-# CALCULATING AVERAGE OF THE CURRENT GRADES & AND ENTERING DESIRED AVERAGE!
+# CALCULATING AVERAGE OF THE CURRENT GRADES & ENTERING DESIRED AVERAGE!
 
 while True:
-    grades = input ("Enter grades to calculate average: ")
+    grades = input ("Enter grades to calculate average (1-5): ")
     if all (grade.isdigit() and 1 <= int(grade) <= 5 for grade in grades.split()):
         break
     print("Invalid value! Please enter valid grades (1-5) separated by spaces!")
@@ -38,43 +39,51 @@ total = sum (grades)
 average = total / number
 print (f"Your current average is: {average}")
 
+# ADDING DATA TO THE LIST "STUDENTS"
+
+students.append ({
+    "Full Name": full_name,
+    "ID" : id_student,
+    "Grades" : grades })
+
+
 while True:
-    desired_average = float(input("Enter your desired average you want to achive: "))
+    desired_average = float(input("Enter the desired average you want to achieve: "))
 
     if desired_average > 5.0:
         print("Desired average cannot be higher than 5.0! Enter value between 1.0 - 5.0!")
         continue
 
     if desired_average < average:
-        print("Desired average cannot be hogher than current average!")
+        print("Desired average cannot be loweer than current average!")
         continue
     
     if desired_average == 5.0 and any(grade != 5 for grade in grades):
-        print("Unfortuantelly it is impossible to achive this average, because you already have grades that are not 5!")
+        print("Unfortunately, it is impossible to achieve this average because you already have grades that are not 5!")
         break
         exit()
     
-    if desired_average > average < 5.0:
+    if desired_average > average and average < 5.0:
        print ("Your target grade and required grades can be calculated. ")
        break
         
     else: 
-       print ("Your current average is already 5.0. Kepep doing good job!")
+       print ("Your current average is already 5.0. Keep doing a good job!")
        exit()
 
          
   
-# (A) CALCULATION OF THE GRADE THAT IS NECCESSARY TO ACHIVE DESIRED AVERAGE GRADE (this value can be number that is not in the range of 1-5)
-# L: target_grade = dessired_average * (number + 1) - total
+# (A) CALCULATION OF THE GRADE THAT IS NECESSARY TO ACHIEVE DESIRED AVERAGE GRADE (this value can be number that is not in the range of 1-5)
+# L: target_grade = desired_average * (number + 1) - total
             
 
 target_grade = (desired_average * (number + 1) - total)
-print (f"Target grade {full_name} needs to achive is: {target_grade}")
+print (f"Target grade {full_name} needs to achive is: {target_grade}!")
 
 
-# (B) CALCULATION OF THE GRADES THAT IS NECESSARY TO ACHIEVE DESIRED AVERAGE GRADE (these values must be numbers between 1-5)
+# (B) CALCULATION OF THE GRADES THAT ARE NECESSARY TO ACHIEVE DESIRED AVERAGE GRADE (these values must be numbers between 1-5)
 # L: check how much of 5s we need to achieve to get the same desired_average or higher (if possible)
-# If desired_average is 5.0, that will be impossible to achive if grades bellow 5 exists
+# If desired_average is 5.0, it will be impossible to achieve if grades below 5 exist
 
     
 if desired_average < 5.0:
@@ -82,3 +91,6 @@ if desired_average < 5.0:
   print(f"You will need at least {required_grades} more grades of 5s to reach your goal!")
 
 
+print("Student Information:")
+for student in students:
+    print(f"Name: {student['Full Name']}, ID: {student['ID']}, Grades: {student['Grades']}")
